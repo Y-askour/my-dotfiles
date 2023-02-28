22cd30b740512aac7cb0b111952ddb0eed445870
Younes Askour <pausedarkf@gmail.com>
2023-02-28 17:15:49 +0100

    Update lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,10 +3,6 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'sumneko_lua',
-  'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
